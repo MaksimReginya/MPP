72ec8228f4815647a724ab585fab60bef7e5ea33
MaxReginya <maksimr1998@mail.ru>
2017-11-03 11:22:28 +0300

    Create viewing xml files

---
+++ 6/XmlEditor/XmlEditor/MainForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace XmlEditor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeTabToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.tabs = new System.Windows.Forms.TabControl();
+            this.mainMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.closeTabToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(815, 24);
+            this.mainMenuStrip.TabIndex = 0;
+            this.mainMenuStrip.Text = "mainMenu";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Enabled = false;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Enabled = false;
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // closeTabToolStripMenuItem
+            // 
+            this.closeTabToolStripMenuItem.Enabled = false;
+            this.closeTabToolStripMenuItem.Name = "closeTabToolStripMenuItem";
+            this.closeTabToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.closeTabToolStripMenuItem.Text = "Close tab";
+            this.closeTabToolStripMenuItem.Click += new System.EventHandler(this.closeTabToolStripMenuItem_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "file.xml";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.FileName = "file.xml";
+            // 
+            // tabs
+            // 
+            this.tabs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabs.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tabs.Location = new System.Drawing.Point(0, 27);
+            this.tabs.Name = "tabs";
+            this.tabs.SelectedIndex = 0;
+            this.tabs.Size = new System.Drawing.Size(810, 369);
+            this.tabs.TabIndex = 1;
+            this.tabs.SelectedIndexChanged += new System.EventHandler(this.tabs_SelectedIndexChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 394);
+            this.Controls.Add(this.tabs);
+            this.Controls.Add(this.mainMenuStrip);
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.MinimumSize = new System.Drawing.Size(500, 432);
+            this.Name = "MainForm";
+            this.Text = "XmlEditor";
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeTabToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.TabControl tabs;
+    }
+}
+
---
+++ 6/XmlEditor/XmlEditor/MainForm.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XmlEditor
+{
+    public partial class MainForm : Form
+    {
+        #region Private fields
+        
+        private XmlDocument _xmlDoc;
+        private TreeView _currentTreeView;
+        
+        #endregion
+
+        #region Constructor
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void OpenFile()
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+                try
+                {
+                    _xmlDoc = new XmlDocument();
+                    _xmlDoc.Load(openFileDialog.FileName);
+                }
+                catch (FileNotFoundException e)
+                {
+                    MessageBox.Show("File can not be found. Please try again");
+                }
+        }
+
+        private void LoadTreeFromXmlDocument()
+        {
+            try
+            {
+                _currentTreeView.Nodes.Clear();
+
+                foreach (XmlNode node in _xmlDoc.DocumentElement.ChildNodes)
+                    AddNode(_currentTreeView.Nodes, node);
+
+                _currentTreeView.ExpandAll();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.InnerException?.ToString() ?? ex.Message);
+            }
+        }
+
+        private static void GetAttributes(XmlNode node, StringBuilder text)
+        {
+            text.AppendFormat(node.Attributes?["name"]?.Value ?? (!node.Name.Equals("#text") ? node.Name : node.InnerText));
+
+            if (node.Attributes == null || node.Attributes.Count < 2)
+                return;
+
+            int i;
+            text.AppendFormat(" (");
+            for (i = 1; i < node.Attributes.Count - 1; i++)
+                text.AppendFormat(node.Attributes[i].Name + "=" + node.Attributes[i].Value + ", ");
+            text.AppendFormat(node.Attributes[i].Name + "=" + node.Attributes[i].Value + ")\n");
+        }
+
+        private static void AddNode(TreeNodeCollection nodes, XmlNode node)
+        {
+            var text = new StringBuilder();
+            GetAttributes(node, text);
+
+            if (node.HasChildNodes)
+            {
+                TreeNode newNode = nodes.Add(text.ToString());
+
+                XmlNodeList childNodes = node.ChildNodes;
+                for (int i = 0; i <= childNodes.Count - 1; i++)
+                {
+                    XmlNode childNode = node.ChildNodes[i];
+                    AddNode(newNode.Nodes, childNode);
+                }
+            }
+            else
+                nodes.Add(text.ToString());
+        }
+
+        private void EditAttribute(TreeNode node)
+        {
+            
+        }
+
+        #endregion
+
+        #region Event handlers
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFile();
+
+                saveAsToolStripMenuItem.Enabled = true;
+                saveToolStripMenuItem.Enabled = true;                
+                var treeView = new TreeView();
+                var page = new TabPage(Path.GetFileName(_xmlDoc.BaseURI))
+                {
+                    Width = tabs.Width,
+                    Height = tabs.Height
+                };
+                treeView.Width = tabs.Width;
+                treeView.Height = tabs.Height;
+                treeView.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top;
+                page.Controls.Add(treeView);
+                tabs.TabPages.Add(page);
+                closeTabToolStripMenuItem.Enabled = true;
+                treeView.MouseDoubleClick += treeView_MouseDoubleClick;
+                _currentTreeView = treeView;
+
+                LoadTreeFromXmlDocument();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error");
+            }
+                                
+        }        
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }                
+
+        private void treeView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            var localTreeView = (TreeView)sender;
+            var node = localTreeView.SelectedNode;
+            EditAttribute(node);
+        }        
+
+        private void closeTabToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (tabs.TabPages.Count > 1)
+            {                
+                tabs.SelectedIndex = tabs.SelectedIndex - 1 > 0 ? tabs.SelectedIndex - 1 : tabs.SelectedIndex;
+                _currentTreeView = tabs.SelectedTab.Controls[0] as TreeView;
+                tabs.TabPages.Remove(tabs.SelectedTab);                                
+            }
+
+            if (tabs.TabPages.Count == 1)
+            {
+                saveAsToolStripMenuItem.Enabled = false;
+                saveToolStripMenuItem.Enabled = false;
+                closeTabToolStripMenuItem.Enabled = false;                                                                
+            }            
+        }
+
+        private void tabs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {                            
+                _currentTreeView = tabs.SelectedTab?.Controls[0] as TreeView;
+            }
+            catch (ArgumentOutOfRangeException) { }
+        }
+
+        #endregion
+    }
+}
